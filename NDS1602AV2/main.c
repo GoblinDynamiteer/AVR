7312465226a2d3156e90ac1f77ae6c7a35a7b1b1
Johan Kämpe <kampe.johan@gmail.com>
2017-04-13 12:13:21 +0200

    Placeholder Atmel Studio project for LCD NDS1602AV2

---
+++ NDS1602AV2/main.c
@@ -0,0 +1,18 @@
+/*
+ * NDS1602AV2.c
+ *
+ * Created: 2017-04-13 12:05:13
+ * Author : Johan Kämpe
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
