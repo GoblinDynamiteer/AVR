760c739e3ad2283ca1299ea44c281ed5f1d3b2f8
Johan Kämpe <kampe.johan@gmail.com>
2017-04-27 21:10:25 +0200

    Updated timer code

--- TIMER/main.c
+++ TIMER/main.c
@@ -5,25 +5,58 @@
  * Author : Johan Kämpe
  */
 
+#define F_CPU 1000000UL
 #include <avr/io.h>
 #include "lib/libavr.h"
 
 #define LEDPIN 28
 
-void timerInit64(void);
+#define TIMER_PRESCALER_OFF (0b00000001)
+#define TIMER_PRESCALER_8 (0b00000010)
+#define TIMER_PRESCALER_64 (0b00000011)
+#define TIMER_PRESCALER_256 (0b00000100)
+#define TIMER_PRESCALER_1024 (0b00000101)
+
+#define GET_COUNTER0 TCNT0
+#define SET_COUNTER0 TCNT0
+
+#define GET_COUNTER1 TCNT1
+#define SET_COUNTER1 TCNT1
+
+void timer0init(uint8_t prescaler);
 
 int main(void){
   pinMode(LEDPIN, OUTPUT);
   setPin(LEDPIN, OFF);
-  timerInit64();
+  timer0init(TIMER_PRESCALER_256);
+  timer1init(TIMER_PRESCALER_8);
+   /*  From EXCEL ~20ms for 8bit timer with 256 prescaler  */
+  //uint8_t timerTrigger = 78, secTicker = 0;
+  uint16_t timerTrigger = 24999, secTicker = 0;
   while(1){
-    if(TCNT1 >= 15624){
+    if(GET_COUNTER1 >= timerTrigger){
+      secTicker++;
+      SET_COUNTER1 = 0;
+    }
+    if(secTicker >= 10){
       togglePin(LEDPIN);
-      TCNT1 = 0;
+      secTicker = 0;
     }
   }
 }
 
-void timerInit64(void){
-  TCCR1B |= ((1 << CS10) | (1 << CS11)); // Set up timer at Fcpu/64
+/*   TIMER0: 8 bit  */
+void timer0init(uint8_t prescaler){
+    /*  Set up Timer/Counter 0 with prescaling   */
+    TCCR0B |= prescaler; //Data sheet page 141
+    /*  Init Timer/Counter 0   */
+    SET_COUNTER0 = 0; //Data sheet page 145
+}
+
+/*   TIMER1: 16 bit  */
+void timer1init(uint8_t prescaler){
+    /*  Set up Timer/Counter 0 with prescaling   */
+    TCCR1B |= prescaler; //Data sheet page 141
+    /*  Init Timer/Counter 0   */
+    SET_COUNTER1 = 0; //Data sheet page 145
 }
