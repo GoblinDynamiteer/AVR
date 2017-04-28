a9439cd9a39e1bef72edcab475c633ca8975aad9
Johan Kämpe <kampe.johan@gmail.com>
2017-04-28 09:04:36 +0200

    PWM testing

--- LEDDIMMER/main.c
+++ LEDDIMMER/main.c
@@ -12,30 +12,49 @@
 #include "lib/libavr.h"
 #define POTPIN ADC0
 #define LEDPIN 28
+#define TRIGGER = 999 //1ms
 
 void dimmer(uint16_t on);
 uint16_t convertValue(uint16_t pv);
 
+uint16_t PWMon = 0;
+uint16_t PWMoff = 0;
+uint8_t OnOff = 1;
+
 int main(void){
  pinMode(LEDPIN, OUTPUT); //LED-output
- uint16_t potValue;
+ uint16_t potValue, onCount = 0, offCount = 0;
  ADCInit();
+ timer1init(TIMER_PRESCALER_OFF); //16bit timer
  while(1){
-  potValue = ADCRead(POTPIN);
-  dimmer(potValue);
- }
-}
+  PWMon = ADCRead(POTPIN) /10 ;
+  PWMoff = 102 - PWMon;
+  uint8_t current = OnOff;
 
-void dimmer(uint16_t on){
-  uint16_t off = 1024-on;
-  setPin(LEDPIN, ON);
-  while(on--){
-    _delay_us(1);
+  if(OnOff){
+    if(GET_COUNTER1 >= TRIGGER){
+      SET_COUNTER1 = 0x0;
+      onCount++;
+    }
+    if(onCount > PWMon){
+      togglePin(LEDPIN)
+      OnOff = 0;
+      onCount = 0;
+    }
   }
-  setPin(LEDPIN, OFF);
-  while(off--){
-    _delay_us(1);
+
+  if(!OnOff){
+    if(GET_COUNTER1 >= TRIGGER){
+      SET_COUNTER1 = 0x0;
+      offCount++;
+    }
+    if(offCount > PWMoff){
+      togglePin(LEDPIN)
+      OnOff = 1;
+      offCount = 0;
+    }
   }
+ }
 }
 
 /*  unused   */
