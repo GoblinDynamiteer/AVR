2c390f50faba48896bf64121adad0a1d688cf093
Johan Kämpe <kampe.johan@gmail.com>
2017-04-29 16:07:06 +0200

    Removed serial debug output

--- BLINKM_RGBLED/main.c
+++ BLINKM_RGBLED/main.c
@@ -9,47 +9,22 @@
 #define BAUD_RATE 2400
 
 #include <avr/io.h>
-#include "lib/i2c.h"
 #include "lib/libavr.h"
 #include "lib/avrblinkm.h"
 
 int main(void){
-  uint8_t success;
   SerialInit(F_CPU/16/BAUD_RATE-1);
   /*   INIT I2C, NO PRESCALING  */
-  //i2c_init();
   i2cInit();
   _delay_us(10);
-  success = blinkM_stopScript();
-  if(success){
-    SerialSendNL("Stopped BlinkM Script!");
-  }
-  else{
-    SerialSendNL("ERROR stopping BlinkM Script!");
-  }
+  blinkM_stopScript();
   blinkM_setFadeSpeed(13);
-
   while(1){
-    if(blinkM_fadeToRGB(0xFF,0x0,0x0)){
-      SerialSendNL("Setting RED");
-    }
-    else{
-      SerialSendNL("Error setting color RED");
-    }
+    blinkM_fadeToRGB(0xFF,0x0,0x0);
     sleep(1000);
-    if(blinkM_fadeToRGB(0x00,0xFF,0x0)){
-      SerialSendNL("Setting GREEN");
-    }
-    else{
-      SerialSendNL("Error setting color GREEN");
-    }
+    blinkM_fadeToRGB(0x00,0xFF,0x0);
     sleep(1000);
-    if(blinkM_fadeToRGB(0x0,0x0,0xFF)){
-      SerialSendNL("Setting BLUE");
-    }
-    else{
-      SerialSendNL("Error setting color BLUE");
-    }
+    blinkM_fadeToRGB(0x0,0x0,0xFF);
     sleep(1000);
   }
   return 0;
